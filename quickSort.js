ade7e18727556b6442259edbe8af147af34f475a
sachin singh <sachin.singh@antino.io>
2020-02-24 18:25:20 +0530

    Adding quickSort in Javascript Algo

---
+++ quickSort.js
@@ -0,0 +1,45 @@
+// first version
+
+// First Version
+function pivot(arr, start=0, end=arr.length+1){
+    function swap(array, i, j) {
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  
+    var pivot = arr[start];
+    var swapIdx = start;
+  
+    for(var i = start + 1; i < arr.length; i++){
+      if(pivot > arr[i]){
+        swapIdx++;
+        swap(arr,swapIdx,i);
+      }
+    }
+    swap(arr,start,swapIdx);
+    return swapIdx;
+  }
+
+
+
+
+
+
+//quick sort algo
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+
+  return arr;
+}
+
+console.log(quickSort([2, 1, 8, 5, 3, 4]));
