98fce8177d4a45fe28c906634f839e4bbf596eaa
sachin singh <sachin.singh@antino.io>
2020-02-27 13:47:36 +0530

    Adding set method javascript Algo

--- doublylinklist.js
+++ doublylinklist.js
@@ -1,76 +1,103 @@
 class Node{
-  constructor(val){
-      this.val = val;
-      this.next = null;
-      this.prev = null;
-  }
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
 }
 
 class DoublyLinkedList{
-  constructor(){
-      this.head = null;
-      this.tail = null;
-      this.length = 0;
-  }
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
 
-  push(val){
-      var newNode = new Node(val)
-      if(this.length === 0){
-          this.head = newNode
-          this.tail = newNode
-      } else {
-          this.tail.next = newNode
-          newNode.prev = this.tail
-          this.tail = newNode
-      }
+    push(val){
+        var newNode = new Node(val)
+        if(this.length === 0){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
 
-      this.length++;
-      return this
+        this.length++;
+        return this
 
-  }
+    }
 
-  pop(){
-      if(!this.head) return undefined;
-      var popedNode = this.tail
-      if(this.length === 1){
-          this.head = null;
-          this.tail = null;
-      } else{
-          this.tail = popedNode.prev
-          this.tail.next = null;
-          popedNode.prev = null;
-      }
-     this.length--
-     return popedNode
+    pop(){
+        if(!this.head) return undefined;
+        var popedNode = this.tail
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else{
+            this.tail = popedNode.prev
+            this.tail.next = null;
+            popedNode.prev = null;
+        }
+       this.length--
+       return popedNode
 
-  }
-  shift(){
-      if(!this.head) return undefined
-      var oldHead = this.head;
-      if(this.length === 1){
-          this.head = null
-          this.tail = null
-      } else {
-      this.head = oldHead.next;
-      this.head.prev = null;
-      oldHead.next = null;
-      }
-      this.length--
-      return oldHead
-  }
-  unshift(val){
-      var newNode = new Node(val)
-      if(this.length === o){
-          this.head = newNode
-          this.tail = newNode
-      } else {
-          this.head.prev = newNode
-          newNode.next = this.head
-          this.head = newNode
-      }
-      this.length++
-      return this;
+    }
+    shift(){
+        if(!this.head) return undefined
+        var oldHead = this.head;
+        if(this.length === 1){
+            this.head = null
+            this.tail = null
+        } else {
+        this.head = oldHead.next;
+        this.head.prev = null;
+        oldHead.next = null;
+        }
+        this.length--
+        return oldHead
+    }
+    unshift(val){
+        var newNode = new Node(val)
+        if(this.length === o){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.head.prev = newNode
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this;
 
-  }
+    }
+
+    get(index){
+        if(index< 0|| index>= this.length) return null
+        if(index<= this.length/2){
+            console.log("working from start")
+          var count = 0;
+        var current = this.head
+        while(count != index){
+            current = current.next
+            count++
+        }
+        return current   
+        }else {
+            console.log("Working from end")
+            var count = this.length -1
+            var current = this.tail
+            while(count !== index){
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+       
+    }
+   
+   
+
 }
 
